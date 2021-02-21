287531c1d7eb50f86b8b2a58421212042591c73f
Laura Carvajal <lauracarvajal@gmail.com>
2021-02-21 13:12:45 +0000

    main as script

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongo = require('./lib/mongo');
-const main = require('./main');
+// const express = require('express');
+// const mongo = require('./lib/mongo');
+// const main = require('./main');
 
-const app = express();
-app.get('/', async (req, res) => res.json(await main.stackClasses()));
+// const app = express();
+// app.get('/', async (req, res) => res.json(await main.stackClasses()));
 
-mongo.client()
-	.then(() => {
-		const PORT = process.env.PORT || 3000
-		return app.listen(PORT, () => {
-			console.log('listening on port', PORT);
-		});
-	})
-	.catch(error => {
-		console.log(error)
-		throw new Error(error);
-	});
+// mongo.client()
+// 	.then(() => {
+// 		const PORT = process.env.PORT || 3000
+// 		return app.listen(PORT, () => {
+// 			console.log('listening on port', PORT);
+// 		});
+// 	})
+// 	.catch(error => {
+// 		console.log(error)
+// 		throw new Error(error);
+// 	});
 
-module.exports = app;
+// module.exports = app;
