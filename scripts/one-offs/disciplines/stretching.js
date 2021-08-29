64abe4719a151a165cf1fbc78f976f342788b3c5
Laura Carvajal <lauracarvajal@gmail.com>
2021-08-29 08:57:14 +0100

    full schedule

--- scripts/one-offs/disciplines/stretching.js
+++ scripts/one-offs/disciplines/stretching.js
@@ -16,6 +16,14 @@ module.exports = [{
 		exclude: ['Post-Ride', 'Warm'],
 		repeatsOk: true, 
 	},{
+		name: 'fullBodyStretch_10',
+		discipline: 'stretching',
+		duration: 10,
+		instructors: {'Adrian Williams': 10, 'Hannah Corbin': 8, 'Matty Maggiacomo': 10, 'Ben Aldis': 10},
+		classType: ['Full Body Stretch'], 
+		exclude: ['Post-Ride', 'Warm'],
+		repeatsOk: true, 
+	},{
 		name: 'postRunStretch_5',
 		discipline: 'stretching',
 		duration: 5,
@@ -30,7 +38,6 @@ module.exports = [{
 // exports.postRunStretch_5 = build('stretching', 5, {'Adrian Williams': 1}, ['Pre & Post-Run Stretch'], ['Post'], null, true, true)
 // exports.armsStretch_10 = build('stretching', 10, {'Andy Speer': 9, 'Ben Aldis': 9, 'Adrian Williams': 10, 'Hannah Corbin': 8}, ['Upper Body Stretch'], ['Arms'], ['Seated', 'Foam', 'Post-Ride', 'Warm'], false, true)
 // exports.chestStretch_10 = build('stretching', 10, {'Andy Speer': 9, 'Ben Aldis': 9, 'Adrian Williams': 10, 'Hannah Corbin': 8}, ['Upper Body Stretch'], ['Chest'], ['Seated', 'Foam', 'Post-Ride', 'Warm'], false, true)
-// exports.fullBodyStretch_10 = build('stretching', 10, {'Andy Speer': 9, 'Matty Maggiacomo': 9, 'Ben Aldis': 9, 'Adrian Williams': 10, 'Hannah Corbin': 8}, ['Full Body Stretch'], null, ['Seated', 'Foam', 'Post-Ride', 'Warm'], false, true)
 // exports.lowerBodyStretch_10 = build('stretching', 10, {'Andy Speer': 9, 'Ben Aldis': 9, 'Adrian Williams': 10, 'Hannah Corbin': 8}, ['Lower Body Stretch'], ['Legs'], ['Seated', 'Foam', 'Post-Ride', 'Warm'], false, true)
 // exports.upperBodyStretch_5 = build('stretching', 5, {'Andy Speer': 9, 'Ben Aldis': 9, 'Adrian Williams': 10, 'Hannah Corbin': 8}, ['Upper Body Stretch'], null, ['Post-Ride', 'Warm', 'Foam'], false, true)
 // exports.lowerBodyStretch_5 = build('stretching', 5, {'Andy Speer': 9, 'Ben Aldis': 9, 'Adrian Williams': 10, 'Hannah Corbin': 8}, ['Lower Body Stretch'], null, ['Post-Ride', 'Warm', 'Foam'], false, true)
