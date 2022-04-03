185b35d1884b754194d516dc5316b1bef68322c9
Laura Carvajal <laura.carvajal@gmail.com>
2022-04-03 11:20:55 +0100

    Update schedule.js

--- scripts/one-offs/schedule.js
+++ scripts/one-offs/schedule.js
@@ -4,47 +4,55 @@ const ComboModel = require('../../models/combo');
 const mongo = require('../../lib/mongo');
 
 const weekA = {
-	  "Friday": [
-	    "fullBodyStrength_30",
-	    "fullBodyStretch_10"
-	  ],
 	  "Monday": [
+	    "standingYoga_5",
+	    "core_5",
+	    "upperBodyStretch_5",
 	    "warmUpRide_5",
-	    "ride_30",
-	    "coolDownRide_5",
+	    "ride_15",
 	    "postRideStretch_5",
 	    "chestStrength_20",
 	    "upperBodyStretch_5"
 	  ],
-	  "Saturday": [
-	    "standingYoga_10",
-	    "recoveryRide_10",
-	    "postRideStretch_5"
-	  ],
-	  "Sunday": [
-	    "standingYoga_10",
-	    "fullBodyStretch_10"
-	  ],
-	  "Thursday": [
+	  "Tuesday": [
+	    "standingYoga_5",
+	    "core_5",
+	    "upperBodyStretch_5",
 	    "warmUpRide_5",
 	    "ride_30",
 	    "coolDownRide_5",
 	    "postRideStretch_5"
 	  ],
-	  "Tuesday": [
-	    "warmUpRide_5",
-	    "ride_45",
-	    "coolDownRide_5",
-	    "postRideStretch_5"
-	  ],
 	  "Wednesday": [
+	    "standingYoga_5",
+	    "core_5",
+	    "upperBodyStretch_5",
 	    "warmUpRide_5",
 	    "ride_20",
 	    "coolDownRide_5",
 	    "postRideStretch_5",
-	    "upperBodyStrength_30",
+	    "armsStrength_20",
 	    "upperBodyStretch_5"
-	  ]	
+	  ],
+	  "Thursday": [
+	    "slowFlowYoga_20"
+	  ],
+	  "Friday": [
+	    "fullBodyStrength_30",
+	    "fullBodyStretch_10"
+	  ],
+	  "Saturday": [
+	    "standingYoga_10",
+	    "recoveryRide_20",
+	    "postRideStretch_5"
+	    "fullBodyStretch_10"
+	  ],
+	  "Sunday": [
+	    "standingYoga_10",
+	    "recoveryRide_20",
+	    "postRideStretch_5"
+	    "fullBodyStretch_10"
+	  ]
 }
 
 const weekB = weekA;
